1962c42ef937e8abcc5baaba4d935843e6911100
joshuanyinaku <jnyinaku2288@gmail.com>
2024-10-01 12:45:43 -0400

    Completed

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -9,30 +9,71 @@
  */
 public class PowerPill
 {
-    // TODO - replace this line with instruction from step 1
+    static int DEFAULT_POWER = 10;
+
 
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    int power;
+    String name;
 
     // constructors
+    /**
+     \* Initializes this power pill to a default power value
+     \* and sets the name of the pill to name.
+     \* @param name the name of this power pill.
+     */
+    public PowerPill(String name){
+        this.name = name;
+        power = DEFAULT_POWER;
+    }
 
-    // TODO - replace this line with instruction from step 4
+    /**
+     * Initializes this power pill to the value of power
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill
+     * @param power the power level of this power pill.
+     */
 
-    // TODO - replace this line with instruction from step 5
+    public PowerPill(String name, int power){
+        this.name = name;
+        this.power = power;
+    }
 
 
     // accessor methods
 
-    // TODO - replace this line with instruction from step 6
+    public static int getPowerPill() {
+        return DEFAULT_POWER;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 
     // mutator methods
 
-    // TODO - replace this line with instruction from step 7
+    public static void setPowerPill(int powerPill) {
+        DEFAULT_POWER = powerPill;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     // toString method
 
-    // TODO - replace this line with instruction from step 8
-
+    @Override
+    public String toString() {
+        return "PowerPill " + name +" = "+power;
+    }
 }
